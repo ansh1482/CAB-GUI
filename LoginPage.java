af527ebbd1e2e408ac4a95786038620c6aa57ad0
ansh1482 <46290844+ansh1482@users.noreply.github.com>
2019-11-05 19:02:15 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,13 +1,18 @@
+package com.main;
+
 import javax.swing.*;
 import com.signUp.*;
+import java.net.*;
 
 
-
-class LoginPage {
+public class LoginPage {
+    public static Socket sk;
     public static void main(String arg[]) {
         try {
-            Login frame = new Login();
+            JFrame frame = new JFrame();
+            Login login = new Login(frame);
             frame.setSize(500,300);
+            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
